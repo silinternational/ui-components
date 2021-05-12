76d96403d6ee3207bebb62ecae9183796a2d74bc
hobbitronics <miguel.dw@gmail.com>
2021-05-12 11:30:03 -0400

    added Badge

---
+++ components/Badge.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let color
+</script>
+
+<style>
+  span {
+    background-color: gray;
+    min-width: 1.15em;
+    min-height: 1.15em;
+    padding: .2em;
+  }
+</style>
+
+<span class="dib align-center white fs-16 br-50" class:blue={color === 'blue'} class:green={color === 'green'} class:skyblue={color === 'skyblue'}><slot /></span>
