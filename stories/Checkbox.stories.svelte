09f9f09a9806e341a577dcef19a6c31b2d26017d
Ryan Garant <rgarant@tenable.com>
2021-05-18 22:09:44 -0700

    add storybook

---
+++ stories/Checkbox.stories.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import { Checkbox } from "../components/mdc";
+</script>
+
+<Meta
+  title="Atoms/Checkbox"
+  component={Checkbox}
+/>
+
+<Template let:args>
+  <Checkbox {...args} on:click={args.onClick}>{args.label}</Checkbox>
+</Template>
+
+<Story
+  raised
+  name="Checked"
+  args={{
+    label: "Checkbox",
+    checked: true
+  }}
+/>
+
+<Story
+  name="Uppercase"
+  args={{
+    label: "Checkbox",
+    uppercase: true
+  }}
+/>
+
+<Story
+  name="Disabled"
+  args={{
+    disabled: true,
+    label: "Checkbox",
+  }}
+/>
