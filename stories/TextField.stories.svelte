633f7c35b9823d6bda4310984438856661dff1b0
hobbitronics <miguel.dw@gmail.com>
2021-05-21 09:48:38 -0400

    created TextField story

---
+++ stories/TextField.stories.svelte
@@ -0,0 +1,70 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import { TextField } from "../components/mdc";
+
+  let title = 'TextField'
+</script>
+
+<Meta
+  title="Atoms/TextField"
+  component={TextField}
+/>
+
+<Template let:args>
+  <TextField {...args}>
+    {args.label}
+  </TextField>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    label: title,
+    value: '',
+    placeholder: '',
+    maxlength: undefined,
+    autofocus: false,
+    disabled: false,
+    class: '',
+  }}
+/>
+
+<Story
+  name="Placeholder"
+  args={{
+    label: title,
+    value: '',
+    placeholder: 'Enter text here',
+    maxlength: undefined,
+    autofocus: false,
+    disabled: false,
+    class: '',
+  }}
+/>
+
+<Story
+  name="Autofocus"
+  args={{
+    label: title,
+    value: '',
+    placeholder: '',
+    maxlength: undefined,
+    autofocus: true,
+    disabled: false,
+    class: '',
+  }}
+/>
+
+<Story
+  name="Disabled"
+  args={{
+    label: title,
+    value: '',
+    placeholder: '',
+    maxlength: undefined,
+    autofocus: false,
+    disabled: true,
+    class: '',
+  }}
+/>
+
