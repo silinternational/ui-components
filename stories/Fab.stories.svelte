8d5b54db0ef3cea9aac3b79139e9c767a18ab02b
hobbitronics <miguel.dw@gmail.com>
2021-05-21 10:25:11 -0400

    created Fab.stories.svelte

---
+++ stories/Fab.stories.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import { Fab } from "../components/mdc";
+</script>
+
+<Meta
+  title="Atoms/Fab"
+  component={Fab}
+/>
+
+<Template let:args>
+  <Fab {...args} on:click={args.onClick}/>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    icon: 'add',
+    label: "Fab",
+    url: '',
+    class: '', //will only work with global class
+  }}
+/>
