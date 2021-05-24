79d3f2aeb4bc85ef63b3be81bc1e30e7bbf06231
hobbitronics <miguel.dw@gmail.com>
2021-05-24 10:44:02 -0400

    added Item.stories.svelte to stories

---
+++ stories/Item.stories.svelte
@@ -0,0 +1,106 @@
+<script>
+  import { Meta as MetaStory, Template, Story } from "@storybook/addon-svelte-csf";
+  import { List } from "../components/mdc";
+</script>
+
+<MetaStory
+  title="Atoms/List.Item"
+  component={List.Item}
+/>
+
+<Template let:args>
+  <List>
+    <List.Item {...args} on:click={args.onClick}>
+    </List.Item>
+  </List>
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    class: '', //will only work with global class
+    activated: false,
+    graphicUrl: '',
+    graphicAlt: '',
+    icon: '',
+    meta: '',
+    primary: '',
+    secondary: '',
+    text: 'Some text',
+    withDivider: false,
+    nonInteractive: false,
+    tabindex: undefined,
+  }}
+/>
+
+<Story
+  name="Primary"
+  args={{
+    class: '', //will only work with global class
+    activated: false,
+    graphicUrl: '',
+    graphicAlt: '',
+    icon: '',
+    meta: '',
+    primary: 'primary',
+    secondary: '',
+    text: '',
+    withDivider: false,
+    nonInteractive: false,
+    tabindex: undefined,
+  }}
+/>
+
+<Story
+  name="Secondary"
+  args={{
+    class: '', //will only work with global class
+    activated: false,
+    graphicUrl: '',
+    graphicAlt: '',
+    icon: '',
+    meta: '',
+    primary: '',
+    secondary: 'secondary',
+    text: '',
+    withDivider: false,
+    nonInteractive: false,
+    tabindex: undefined,
+  }}
+/>
+
+<Story
+  name="Meta"
+  args={{
+    class: '', //will only work with global class
+    activated: false,
+    graphicUrl: '',
+    graphicAlt: '',
+    icon: '',
+    meta: 'meta',
+    primary: '',
+    secondary: '',
+    text: '',
+    withDivider: false,
+    nonInteractive: false,
+    tabindex: undefined,
+  }}
+/>
+
+<Story
+  name="nonInteractive"
+  args={{
+    class: '', //will only work with global class
+    activated: false,
+    graphicUrl: '',
+    graphicAlt: '',
+    icon: '',
+    meta: 'meta',
+    primary: '',
+    secondary: '',
+    text: '',
+    withDivider: false,
+    nonInteractive: true,
+    tabindex: undefined,
+  }}
+/>
