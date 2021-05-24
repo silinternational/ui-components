4f7988aa674c1f82708d5c26a090d0120ae70e0b
hobbitronics <miguel.dw@gmail.com>
2021-05-24 16:19:58 -0400

    used copyAndModifyArgs in Card.stories.svelte

--- stories/Card.stories.svelte
+++ stories/Card.stories.svelte
@@ -1,8 +1,16 @@
 <script>
-  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
-  import { Button, Card } from "../components/mdc";
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf"
+  import { Button, Card } from "../components/mdc"
+  import { copyAndModifyArgs } from "./helpers.js"
 
   let title = 'Card'
+
+  const args = {
+    outlined: false,
+    class: '',
+    label: title,
+    secondary: false
+  }
 </script>
 
 <Meta
@@ -17,30 +25,23 @@
 </Template>
 
 <Story
-  name="outlined"
-  args={{
-    outlined: true,
-    class: '',
-    label: title,
-  }}
+  name="Default"
+  {args}
+/>
+
+<Story
+  name="Outlined"
+  args={copyAndModifyArgs(args, {outlined: true})}
 />
 
 <Story
   name="Secondary"
-  args={{
-    secondary: "secondary",
-    class: '',
-    label: title,
-  }}
+  args={copyAndModifyArgs(args, {secondary: "secondary"})}
 />
 
 <Story
   name="Color"
-  args={{
-    color: "yellow",
-    class: '',
-    label: title,
-  }}
+  args={copyAndModifyArgs(args, {color: "yellow"})}
 />
 
 <Story let:args name="actions">
