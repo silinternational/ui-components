46453c183811053e2d8b1c5f1dc1b951e78166fa
hobbitronics <miguel.dw@gmail.com>
2021-05-24 16:12:46 -0400

    used copyAndModifyArgs in Checkbox.stories.svelte

--- stories/Checkbox.stories.svelte
+++ stories/Checkbox.stories.svelte
@@ -1,6 +1,14 @@
 <script>
-  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
-  import { Checkbox } from "../components/mdc";
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf"
+  import { Checkbox } from "../components/mdc"
+  import { copyAndModifyArgs } from "./helpers.js"
+
+  const args = {
+    label: "Checkbox",
+    checked: false,
+    disable: false,
+    uppercase: false,
+  }
 </script>
 
 <Meta
@@ -13,26 +21,21 @@
 </Template>
 
 <Story
-  raised
+  name="Default"
+  {args}
+/>
+
+<Story
   name="Checked"
-  args={{
-    label: "Checkbox",
-    checked: true
-  }}
+  args={copyAndModifyArgs(args, {checked: true})}
 />
 
 <Story
   name="Uppercase"
-  args={{
-    label: "Checkbox",
-    uppercase: true
-  }}
+  args={copyAndModifyArgs(args, {uppercase: true})}
 />
 
 <Story
   name="Disabled"
-  args={{
-    disabled: true,
-    label: "Checkbox",
-  }}
+  args={copyAndModifyArgs(args, {disabled: true})}
 />
