135239d2d30ccb1f59c39250e2ae21cd1275cfb0
hobbitronics <miguel.dw@gmail.com>
2021-05-25 13:26:01 -0400

    added TopAppBar.stories.svelte

---
+++ stories/TopAppBar.stories.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf"
+  import { TopAppBar } from "../components/mdc"
+  import { copyAndModifyArgs } from "./helpers.js"
+
+  const args = {
+    class: '', //only works for global classes
+    bgColorIsVariant: false,
+    onNav: () => {},
+  }
+</script>
+
+<Meta
+  title="Atoms/TopAppBar"
+  component={TopAppBar}
+/>
+
+<Template let:args>
+  <TopAppBar {...args} on:nav={args.onNav}/>
+</Template>
+
+<Story
+  name="Default"
+  {args}
+/>
+
+<Story
+  name="Dense"
+  args={copyAndModifyArgs(args, {dense: true})}
+/>
+
+<Story
+  name="Fixed"
+  args={copyAndModifyArgs(args, {fixed: true})}
+/>
+
+<Story
+  name="Hide above tablet"
+  args={copyAndModifyArgs(args, {navIconBreakpointClass: "hide-above-tablet"})}
+/>
