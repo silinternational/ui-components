3c897bb28031048212cd0738f948a41920117acf
hobbitronics <miguel.dw@gmail.com>
2021-05-26 14:19:34 -0400

    added Drawer.stories.svelte

---
+++ stories/Drawer.stories.svelte
@@ -0,0 +1,68 @@
+<script>
+  import { Page } from "../"
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf"
+  import { Drawer } from "../components/mdc"
+  import { copyAndModifyArgs } from "./helpers.js"
+  import './_index.scss'
+
+  const args = {
+    class: '', //only works for global classes
+    title: 'Title',
+    menuItems: [
+      {},
+			{
+				url: './',
+				label: 'see our components in storybook'
+			},
+      {
+        label: '--break--',
+      },
+			{
+				url: './',
+				label: 'see our components in storybook'
+			},
+      {
+				url: './',
+				label: 'see our components in storybook'
+			},
+      {
+        label: '--break--',
+      },
+			{
+				url: './',
+				label: 'see our components in storybook'
+			}
+		]
+  }
+</script>
+
+<Meta
+  title="Molecule/Drawer"
+  component={Drawer}
+/>
+
+<Template let:args>
+  <Drawer {...args}>
+    <span class="pointer" slot="header">
+      <img class="w-100" src="/logo.png" alt="logo">
+    </span>
+
+    <Page title="title" layout="grid">
+      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+        <p>
+          Here is where you put your page content.
+        </p>
+      </div>
+    </Page>
+  </Drawer>
+</Template>
+
+<Story
+  name="Default"
+  {args}
+/>
+
+<Story
+  name="Subtitle"
+  args={copyAndModifyArgs(args, {subtitle: 'subtitle'})}
+/>
