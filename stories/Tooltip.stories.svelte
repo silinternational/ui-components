442830bb7a58b78ea017ce41cd3170b10b67477f
hobbitronics <miguel.dw@gmail.com>
2021-05-26 11:03:37 -0400

    added Tooltip.stories.svelte

---
+++ stories/Tooltip.stories.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf"
+  import { Tooltip } from "../components/mdc"
+  import { copyAndModifyArgs } from "./helpers.js"
+
+  const args = {
+    tooltipID: '1',
+    class: '', //only works for global classes
+  }
+</script>
+
+<Meta
+  title="Atoms/Tooltip"
+  component={Tooltip}
+/>
+
+<Template let:args>
+  <div class="flex justify-center my-4">
+    <Tooltip.Wrapper ariaDescribedBy={args.tooltipID}>wrapper slot</Tooltip.Wrapper>
+  </div>
+
+  <Tooltip {...args}>Tooltip main slot here</Tooltip>
+</Template>
+
+<Story
+  name="Default"
+  {args}
+/>
+
+<Story
+  name="Position"
+  args={
+    copyAndModifyArgs(args, {positionX: 'start', positionY: 'above'})
+  }
+/>
