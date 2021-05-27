d80dc5771b946dc2611327df7523f3269f1a7827
hobbitronics <miguel.dw@gmail.com>
2021-05-27 14:08:18 -0400

    added Badge.stories.svelte

---
+++ stories/Badge.stories.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf"
+  import { Badge } from "../"
+  import { copyAndModifyArgs } from "./helpers.js"
+
+  const args = {
+    class: '', //only works for global classes
+    color: 'gray'
+  }
+</script>
+
+<Meta
+  title="Atoms/Badge"
+  component={Badge}
+/>
+
+<Template let:args>
+  <Badge {...args}>S</Badge>
+</Template>
+
+<Story
+  name="Default"
+  {args}
+/>
+
+<Story
+  name="Blue"
+  args={copyAndModifyArgs(args, {color: 'blue'})}
+/>
