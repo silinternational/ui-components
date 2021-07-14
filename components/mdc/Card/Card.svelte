14a1e806c339e5bf51c66b70940620a0976ab340
hobbitronics <miguel.dw@gmail.com>
2021-07-14 11:45:47 -0400

    added noPadding prop to Card

--- components/mdc/Card/Card.svelte
+++ components/mdc/Card/Card.svelte
@@ -4,6 +4,7 @@ export let secondary = ''
 export let outlined = false
 export let color = 'white'
 export let isClickable = false
+export let noPadding = false
 
 $: tabindex = isClickable ? '0' : undefined
 </script>
@@ -19,6 +20,10 @@ $: tabindex = isClickable ? '0' : undefined
   padding-left: 16px;
   padding-right: 16px;
 }
+.noPadding {
+  padding-left: 0;
+  padding-right: 0;
+}
 </style>
 
 <div {tabindex} class="mdc-card mdc-typography {$$props.class}" style="background-color: {color}" class:mdc-card--outlined={outlined} class:mdc-card__primary-action={isClickable} on:click on:mousedown on:mouseup on:keypress>
@@ -27,7 +32,7 @@ $: tabindex = isClickable ? '0' : undefined
       {secondary}
     </div>
   {/if}
-  <div class="supporting-text h-100">
+  <div class="supporting-text h-100" class:noPadding>
     <slot />
   </div>
   {#if $$slots.actions}
