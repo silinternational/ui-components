f2570ead738115e804e486b191a0f0902c9dda35
hobbitronics <miguel.dw@gmail.com>
2021-07-21 13:32:02 -0500

    missing '#'

--- components/Badge.svelte
+++ components/Badge.svelte
@@ -9,7 +9,7 @@
   $: if (/^#([0-9A-F]{3}){1,2}$/i.test(color)) {
     color.length == 7 && (transparentColor = color + '12')
 
-    color.length == 4 && (transparentColor = color.slice(1, 2).repeat(2) + color.slice(2, 3).repeat(2) + color.slice(3, 4).repeat(2) + '12')
+    color.length == 4 && (transparentColor = '#' + color.slice(1, 2).repeat(2) + color.slice(2, 3).repeat(2) + color.slice(3, 4).repeat(2) + '12')
   }
 </script>
 
