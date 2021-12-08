d0d747c58b8f7f627a95503de4e592febe4eeed3
Russell <russellchamp@gmail.com>
2021-12-08 10:06:50 -0500

    Update components/mdc/TextInput/TextField.svelte

--- components/mdc/TextInput/TextField.svelte
+++ components/mdc/TextInput/TextField.svelte
@@ -29,7 +29,7 @@ const focus = (node) => autofocus && node.focus()
 
 <style>
 .required {
-  color: var(--mdc-required-input);
+  color: var(--mdc-required-input, var(--mdc-theme-status-error));
   font-size: small;
   margin-left: 1rem;
   margin-top: 0.2rem;
