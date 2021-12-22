1a209f976abadaf9e5e8f6e543648278c75c1937
Michael Wilson <michael_wilson@sil.org>
2021-12-21 19:00:32 -0500

    fix(Textinput): fix error on reading 'remove'

--- components/mdc/TextInput/index.js
+++ components/mdc/TextInput/index.js
@@ -6,5 +6,5 @@ import MoneyInput from './MoneyInput.svelte'
 export { TextArea, TextField, MoneyInput }
 
 export const addOrRemoveInvalidClass = async (isInvalid, element) => {
-  isInvalid ? element.classList.add('mdc-text-field--invalid') : element.classList.remove('mdc-text-field--invalid')
+  isInvalid ? element.classList?.add('mdc-text-field--invalid') : element.classList?.remove('mdc-text-field--invalid')
 }
