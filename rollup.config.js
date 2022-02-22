8e7c0159f003b05eff062cfe8961358faf75ecd2
Michael Wilson <michael_wilson@sil.org>
2022-02-22 14:46:21 +0930

    add rollup.config & add deps to roll components

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import pkg from './package.json'
+import resolve from '@rollup/plugin-node-resolve'
+import postcss from 'rollup-plugin-postcss'
+import svelte from 'rollup-plugin-svelte'
+
+const name = pkg.name
+	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+	.replace(/^\w/, m => m.toUpperCase())
+	.replace(/-\w/g, m => m[1].toUpperCase());
+
+export default {
+	input: 'index.js',
+	output: [
+		{ file: pkg.module, 'format': 'es' },
+		{ file: pkg.main, 'format': 'umd', name }
+	],
+	plugins: [
+		svelte(
+            {
+                emitCss: true, // give component style to postcss() for processing
+            }
+        ),
+		resolve(),
+        postcss({
+            extract: false, // create a css file alongside the output.file
+			use: {
+				sass: {
+					includePaths: ['node_modules']
+				}
+			},
+		}),
+	]
+};
