3718ec3659b1d04e01d0e639e373bfb46323f6b7
Michael Wilson <michael_wilson@sil.org>
2022-02-23 12:57:51 +0930

    docs(storybook): load material icons for stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,3 +9,15 @@ export const parameters = {
     },
   },
 }
+
+loadIcons()
+
+// https://material.io/resources/icons
+function loadIcons() {
+  const link = document.createElement('link')
+
+  link.href = 'https://fonts.googleapis.com/icon?family=Material+Icons&display=swap'
+  link.rel = 'stylesheet'
+
+  document.head.appendChild(link)
+}
