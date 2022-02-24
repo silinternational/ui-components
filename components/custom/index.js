a48ae474a515804e581b0573945dbb1f595ac9c1
Michael Wilson <michael_wilson@sil.org>
2022-02-25 08:07:02 +0930

    fix(index): sveltkit error for non indexed components

---
+++ components/custom/index.js
@@ -0,0 +1,7 @@
+import Badge from './Badge.svelte'
+import CustomCard from './CustomCard.svelte'
+import Form from './Form.svelte'
+import StaticChip from './StaticChip.svelte'
+import Tour from './Tour.svelte'
+
+export { Badge, CustomCard, Form, StaticChip, Tour }
