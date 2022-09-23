053af32385578630d8253f5feee9cf5097baa2ab
Michael Wilson <michael_wilson@sil.org>
2022-09-23 17:34:02 +0930

    feat(SearchableSelect): add required prop

--- stories/SearchableSelect.stories.svelte
+++ stories/SearchableSelect.stories.svelte
@@ -1,6 +1,7 @@
 <script>
 import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
 import { SearchableSelect } from '../components/custom'
+import { Button } from '../components/mdc'
 import { copyAndModifyArgs } from './helpers.js'
 
 const args = {
@@ -12,6 +13,7 @@ const args = {
   },
   placeholder: 'choice 1',
   padding: '12px',
+  required: false,
   choice: 'choice 1',
   width: '232px',
   maxlength: 255,
@@ -26,7 +28,10 @@ const args = {
 <Meta title="Atoms/SearchableSelect" component={SearchableSelect} />
 
 <Template let:args>
-  <SearchableSelect {...args} on:chosen={args['on:chosen']} on:check={args['on:check']} />
+  <form on:submit={() => console.log('submitted')}>
+    <SearchableSelect {...args} on:chosen={args['on:chosen']} on:check={args['on:check']} />
+    <Button>Submit</Button>
+  </form>
 </Template>
 
 <Story name="Default" {args} />
@@ -35,3 +40,7 @@ const args = {
 <Story name="Disabled" args={copyAndModifyArgs(args, { disabled: true })} />
 <Story name="Maxlength" args={copyAndModifyArgs(args, { maxlength: 25 })} />
 <Story name="padding" args={copyAndModifyArgs(args, { padding: '20px' })} />
+<Story
+  name="required"
+  args={copyAndModifyArgs(args, { required: true, 'on:check': () => console.log('this is required') })}
+/>
