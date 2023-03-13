6dd75cff01ebc503890de33397c847f697eb019a
Michael Wilson <michael_wilson@sil.org>
2023-03-13 18:03:11 +0930

    Docs(Form): remove success from Form story index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -309,7 +309,6 @@ declare module '@silintl/ui-components' {
   interface FormProps extends svelte.JSX.HTMLAttributes<HTMLElementTagNameMap['div']> {
     id?: string
     saveToLocalStorage?: boolean
-    success?: boolean
   }
   export class Form extends SvelteComponentTyped<FormProps> {}
 
