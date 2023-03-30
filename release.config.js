932c46c28dcee77c702f040f2068f8a293b705e7
Michael Wilson <michael_wilson@sil.org>
2023-03-30 12:10:13 +0930

    add header to changelog config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 const { execSync } = require('child_process')
 
+const CHANGELOG_HEADER = `# Changelog
+All notable changes to this project will be documented in this file.
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html), enforced with [semantic-release](https://github.com/semantic-release/semantic-release).`
+
 const plugins = [
   [
     '@semantic-release/commit-analyzer',
@@ -149,7 +153,7 @@ function getCIConfig() {
     branches: ['main'],
     plugins: [
       ...plugins,
-      '@semantic-release/changelog',
+      ['@semantic-release/changelog', { changelogTitle: CHANGELOG_HEADER }],
       '@semantic-release/npm',
       '@semantic-release/git',
       '@semantic-release/github',
