bf1bbef33a7514b5300440d2e142474e3ed40eae
Michael Wilson <michael_wilson@sil.org>
2023-09-11 11:58:14 +0930

    add(DataTable): add lowContrast prop to DataRowItem

--- components/mdc/Datatable/DataRowItem.svelte
+++ components/mdc/Datatable/DataRowItem.svelte
@@ -1,8 +1,32 @@
 <script>
+/**
+ * @prop {boolean}
+ * @description Whether the cell contains numeric data.
+ */
 export let numeric = false
+/**
+ * @prop {number}
+ * @description The number of columns the cell spans.
+ */
 export let colspan = 1
+/**
+ * @prop {boolean}
+ * @description Whether the cell has low contrast.
+ */
+export let lowContrast = false
 </script>
 
-<td class="mdc-data-table__cell {$$props.class || ''}" class:mdc-data-table__cell--numeric={numeric} {colspan}
-  ><slot /></td
+<style>
+.lowContrast {
+  color: var(--mdc-theme-text-secondary-on-light);
+}
+</style>
+
+<td
+  class="mdc-data-table__cell {$$props.class || ''}"
+  class:lowContrast
+  class:mdc-data-table__cell--numeric={numeric}
+  {colspan}
 >
+  <slot />
+</td>
