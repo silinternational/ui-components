6afbe8153b42d2ccabb8f757f88500ce8cdc390e
hobbitronics <hobbitronics@users.noreply.github.com>
2024-04-09 23:05:40 +0000

    deploy: 4a528660d75fdd3a725ae4012bb2a27f424b1e7e

---
+++ assets/Drawer.stories-34c02b05.js
@@ -0,0 +1,105 @@
+import{S as z,i as G,s as J,c as d,a as D,m as $,b as c,t as f,d as g,e as p,f as _,g as K,k as P,l as F,A as W,z as U,j as h,h as X}from"./index-88099bba.js";import{g as Y,a as Z}from"./spread-8a54911c.js";import{p as x,M as ee,u as L,T as te,S as N,B as j,k as ae,d as v}from"./collect-stories-eabb8f8b.js";import{c as S}from"./helpers-5fa58f44.js";function se(r){let e;return{c(){e=h("Toggle drawer")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function ne(r){let e;return{c(){e=h("Name")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function ie(r){let e;return{c(){e=h("Date")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function ue(r){let e,s,a,o;return e=new v.Header.Item({props:{class:"w-50",$$slots:{default:[ne]},$$scope:{ctx:r}}}),a=new v.Header.Item({props:{$$slots:{default:[ie]},$$scope:{ctx:r}}}),{c(){d(e.$$.fragment),s=D(),d(a.$$.fragment)},m(t,n){$(e,t,n),c(t,s,n),$(a,t,n),o=!0},p(t,n){const u={};n&8&&(u.$$scope={dirty:n,ctx:t}),e.$set(u);const i={};n&8&&(i.$$scope={dirty:n,ctx:t}),a.$set(i)},i(t){o||(f(e.$$.fragment,t),f(a.$$.fragment,t),o=!0)},o(t){g(e.$$.fragment,t),g(a.$$.fragment,t),o=!1},d(t){t&&p(s),_(e,t),_(a,t)}}}function re(r){let e;return{c(){e=h("item")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function le(r){let e;return{c(){e=h("today")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function oe(r){let e,s,a,o;return e=new v.Data.Row.Item({props:{$$slots:{default:[re]},$$scope:{ctx:r}}}),a=new v.Data.Row.Item({props:{$$slots:{default:[le]},$$scope:{ctx:r}}}),{c(){d(e.$$.fragment),s=D(),d(a.$$.fragment)},m(t,n){$(e,t,n),c(t,s,n),$(a,t,n),o=!0},p(t,n){const u={};n&8&&(u.$$scope={dirty:n,ctx:t}),e.$set(u);const i={};n&8&&(i.$$scope={dirty:n,ctx:t}),a.$set(i)},i(t){o||(f(e.$$.fragment,t),f(a.$$.fragment,t),o=!0)},o(t){g(e.$$.fragment,t),g(a.$$.fragment,t),o=!1},d(t){t&&p(s),_(e,t),_(a,t)}}}function me(r){let e;return{c(){e=h("item2")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function ce(r){let e;return{c(){e=h("tomorrow")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function pe(r){let e,s,a,o;return e=new v.Data.Row.Item({props:{$$slots:{default:[me]},$$scope:{ctx:r}}}),a=new v.Data.Row.Item({props:{$$slots:{default:[ce]},$$scope:{ctx:r}}}),{c(){d(e.$$.fragment),s=D(),d(a.$$.fragment)},m(t,n){$(e,t,n),c(t,s,n),$(a,t,n),o=!0},p(t,n){const u={};n&8&&(u.$$scope={dirty:n,ctx:t}),e.$set(u);const i={};n&8&&(i.$$scope={dirty:n,ctx:t}),a.$set(i)},i(t){o||(f(e.$$.fragment,t),f(a.$$.fragment,t),o=!0)},o(t){g(e.$$.fragment,t),g(a.$$.fragment,t),o=!1},d(t){t&&p(s),_(e,t),_(a,t)}}}function de(r){let e;return{c(){e=h("Done loading")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function $e(r){let e,s;return e=new v.Data.Row.Item({props:{colspan:6,$$slots:{default:[de]},$$scope:{ctx:r}}}),{c(){d(e.$$.fragment)},m(a,o){$(e,a,o),s=!0},p(a,o){const t={};o&8&&(t.$$scope={dirty:o,ctx:a}),e.$set(t)},i(a){s||(f(e.$$.fragment,a),s=!0)},o(a){g(e.$$.fragment,a),s=!1},d(a){_(e,a)}}}function fe(r){let e,s,a,o,t,n;return e=new v.Data.Row({props:{$$slots:{default:[oe]},$$scope:{ctx:r}}}),a=new v.Data.Row({props:{$$slots:{default:[pe]},$$scope:{ctx:r}}}),t=new v.Data.Row({props:{$$slots:{default:[$e]},$$scope:{ctx:r}}}),{c(){d(e.$$.fragment),s=D(),d(a.$$.fragment),o=D(),d(t.$$.fragment)},m(u,i){$(e,u,i),c(u,s,i),$(a,u,i),c(u,o,i),$(t,u,i),n=!0},p(u,i){const m={};i&8&&(m.$$scope={dirty:i,ctx:u}),e.$set(m);const w={};i&8&&(w.$$scope={dirty:i,ctx:u}),a.$set(w);const q={};i&8&&(q.$$scope={dirty:i,ctx:u}),t.$set(q)},i(u){n||(f(e.$$.fragment,u),f(a.$$.fragment,u),f(t.$$.fragment,u),n=!0)},o(u){g(e.$$.fragment,u),g(a.$$.fragment,u),g(t.$$.fragment,u),n=!1},d(u){u&&(p(s),p(o)),_(e,u),_(a,u),_(t,u)}}}function ge(r){let e,s,a,o;return e=new v.Header({props:{$$slots:{default:[ue]},$$scope:{ctx:r}}}),a=new v.Data({props:{$$slots:{default:[fe]},$$scope:{ctx:r}}}),{c(){d(e.$$.fragment),s=D(),d(a.$$.fragment)},m(t,n){$(e,t,n),c(t,s,n),$(a,t,n),o=!0},p(t,n){const u={};n&8&&(u.$$scope={dirty:n,ctx:t}),e.$set(u);const i={};n&8&&(i.$$scope={dirty:n,ctx:t}),a.$set(i)},i(t){o||(f(e.$$.fragment,t),f(a.$$.fragment,t),o=!0)},o(t){g(e.$$.fragment,t),g(a.$$.fragment,t),o=!1},d(t){t&&p(s),_(e,t),_(a,t)}}}function _e(r){let e,s,a,o,t,n,u,i;return u=new v({props:{$$slots:{default:[ge]},$$scope:{ctx:r}}}),u.$on("sorted",function(){X(r[0].onSorted)&&r[0].onSorted.apply(this,arguments)}),{c(){e=P("h1"),e.textContent="Title",s=D(),a=P("p"),a.textContent=`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+          dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
+          Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+          sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.
+          Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.
+          Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus,
+          iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.
+          Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed
+          lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla
+          facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien.
+          Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus
+          luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget
+          diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia
+          molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum
+          sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum
+          tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est
+          pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a
+          tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet,
+          augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet.`,o=D(),t=P("p"),t.textContent=`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+          dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
+          Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+          sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.
+          Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.
+          Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus,
+          iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.
+          Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed
+          lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla
+          facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien.`,n=D(),d(u.$$.fragment)},m(m,w){c(m,e,w),c(m,s,w),c(m,a,w),c(m,o,w),c(m,t,w),c(m,n,w),$(u,m,w),i=!0},p(m,w){r=m;const q={};w&8&&(q.$$scope={dirty:w,ctx:r}),u.$set(q)},i(m){i||(f(u.$$.fragment,m),i=!0)},o(m){g(u.$$.fragment,m),i=!1},d(m){m&&(p(e),p(s),p(a),p(o),p(t),p(n)),_(u,m)}}}function be(r){let e,s,a,o,t,n;function u(){return r[2](r[0])}return e=new j({props:{$$slots:{default:[se]},$$scope:{ctx:r}}}),e.$on("click",u),a=new ae({props:{$$slots:{default:[_e]},$$scope:{ctx:r}}}),{c(){d(e.$$.fragment),s=D(),d(a.$$.fragment),o=D(),t=P("footer"),t.innerHTML='<a class="pr-1 svelte-nf961x" href="">Terms of Service</a> <a href="" class="svelte-nf961x">Privacy Policy</a>',F(t,"class","svelte-nf961x")},m(i,m){$(e,i,m),c(i,s,m),$(a,i,m),c(i,o,m),c(i,t,m),n=!0},p(i,m){r=i;const w={};m&8&&(w.$$scope={dirty:m,ctx:r}),e.$set(w);const q={};m&9&&(q.$$scope={dirty:m,ctx:r}),a.$set(q)},i(i){n||(f(e.$$.fragment,i),f(a.$$.fragment,i),n=!0)},o(i){g(e.$$.fragment,i),g(a.$$.fragment,i),n=!1},d(i){i&&(p(s),p(o),p(t)),_(e,i),_(a,i)}}}function we(r){let e;return{c(){e=h("Toggle drawer")},m(s,a){c(s,e,a)},d(s){s&&p(e)}}}function De(r){let e,s,a,o,t,n;function u(){return r[1](r[0])}return t=new j({props:{$$slots:{default:[we]},$$scope:{ctx:r}}}),t.$on("click",u),{c(){e=P("span"),s=P("img"),o=D(),d(t.$$.fragment),F(s,"class","w-100"),W(s.src,a="/logo.png")||F(s,"src",a),F(s,"alt","logo"),F(e,"class","pointer"),F(e,"slot","header")},m(i,m){c(i,e,m),U(e,s),U(e,o),$(t,e,null),n=!0},p(i,m){r=i;const w={};m&8&&(w.$$scope={dirty:m,ctx:r}),t.$set(w)},i(i){n||(f(t.$$.fragment,i),n=!0)},o(i){g(t.$$.fragment,i),n=!1},d(i){i&&p(e),_(t)}}}function qe(r){let e,s,a;const o=[r[0]];let t={$$slots:{header:[De],default:[be]},$$scope:{ctx:r}};for(let n=0;n<o.length;n+=1)t=K(t,o[n]);return s=new L({props:t}),{c(){e=P("body"),d(s.$$.fragment)},m(n,u){c(n,e,u),$(s,e,null),a=!0},p(n,u){const i=u&1?Y(o,[Z(n[0])]):{};u&9&&(i.$$scope={dirty:u,ctx:n}),s.$set(i)},i(n){a||(f(s.$$.fragment,n),a=!0)},o(n){g(s.$$.fragment,n),a=!1},d(n){n&&p(e),_(s)}}}function ve(r){let e,s,a,o,t,n,u,i,m,w,q,T,M,A,I,R;return e=new ee({props:{title:"Molecule/Drawer",component:L}}),a=new te({props:{$$slots:{default:[qe,({args:l})=>({0:l}),({args:l})=>l?1:0]},$$scope:{ctx:r}}}),t=new N({props:{name:"Default",args:r[0]}}),u=new N({props:{name:"Subtitle",args:S(r[0],{subtitle:"subtitle"})}}),m=new N({props:{name:"Dismissible",args:S(r[0],{dismissible:!0})}}),q=new N({props:{name:"HideForTablets",args:S(r[0],{hideForTablet:!0,modal:!0})}}),M=new N({props:{name:"hideForPhonesOnly",args:S(r[0],{hideForPhonesOnly:!0,modal:!0})}}),I=new N({props:{name:"miniMenu",args:S(r[0],{miniMenu:!0})}}),{c(){d(e.$$.fragment),s=D(),d(a.$$.fragment),o=D(),d(t.$$.fragment),n=D(),d(u.$$.fragment),i=D(),d(m.$$.fragment),w=D(),d(q.$$.fragment),T=D(),d(M.$$.fragment),A=D(),d(I.$$.fragment)},m(l,b){$(e,l,b),c(l,s,b),$(a,l,b),c(l,o,b),$(t,l,b),c(l,n,b),$(u,l,b),c(l,i,b),$(m,l,b),c(l,w,b),$(q,l,b),c(l,T,b),$(M,l,b),c(l,A,b),$(I,l,b),R=!0},p(l,[b]){const H={};b&9&&(H.$$scope={dirty:b,ctx:l}),a.$set(H);const k={};b&1&&(k.args=l[0]),t.$set(k);const V={};b&1&&(V.args=S(l[0],{subtitle:"subtitle"})),u.$set(V);const y={};b&1&&(y.args=S(l[0],{dismissible:!0})),m.$set(y);const O={};b&1&&(O.args=S(l[0],{hideForTablet:!0,modal:!0})),q.$set(O);const Q={};b&1&&(Q.args=S(l[0],{hideForPhonesOnly:!0,modal:!0})),M.$set(Q);const B={};b&1&&(B.args=S(l[0],{miniMenu:!0})),I.$set(B)},i(l){R||(f(e.$$.fragment,l),f(a.$$.fragment,l),f(t.$$.fragment,l),f(u.$$.fragment,l),f(m.$$.fragment,l),f(q.$$.fragment,l),f(M.$$.fragment,l),f(I.$$.fragment,l),R=!0)},o(l){g(e.$$.fragment,l),g(a.$$.fragment,l),g(t.$$.fragment,l),g(u.$$.fragment,l),g(m.$$.fragment,l),g(q.$$.fragment,l),g(M.$$.fragment,l),g(I.$$.fragment,l),R=!1},d(l){l&&(p(s),p(o),p(n),p(i),p(w),p(T),p(A)),_(e,l),_(a,l),_(t,l),_(u,l),_(m,l),_(q,l),_(M,l),_(I,l)}}}function Se(r,e,s){return[{class:"",title:"Title",toggle:!0,currentUrl:"./user",menuItems:[{},{url:"./user",icon:"account_circle",label:"account",tooltip:"user account"},{url:"./",icon:"face",label:"bio"},{url:"./",icon:"add",label:"add stuff",tooltip:"add things"},{url:"./",icon:"remove",label:"remove stuff",tooltip:"remove things"},{url:"./",icon:"money",label:"reload",button:!0}]},n=>s(0,n.toggle=!n.toggle,n),n=>s(0,n.toggle=!n.toggle,n)]}class E extends z{constructor(e){super(),G(this,e,Se,ve,J,{})}}E.__docgen={version:3,name:"Drawer.stories.svelte",data:[],computed:[],methods:[],components:[],description:null,keywords:[],events:[],slots:[],refs:[]};const C=x(E,{meta:{title:"Molecule/Drawer"},stories:{"tpl:default":{name:"default",template:!0,source:`<body>
+  <Drawer {...args}>
+    <span class="pointer" slot="header">
+      <img class="w-100" src="/logo.png" alt="logo" />
+      <Button on:click={() => (args.toggle = !args.toggle)}>Toggle drawer</Button>
+    </span>
+
+    <Button on:click={() => (args.toggle = !args.toggle)}>Toggle drawer</Button>
+    <Page>
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+        dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
+        Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+        sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.
+        Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.
+        Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus,
+        iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.
+        Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+        aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed
+        lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla
+        facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien.
+        Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus
+        luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget
+        diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia
+        molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum
+        sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum
+        tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est
+        pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a
+        tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet,
+        augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante
+        dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris.
+        Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+        sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.
+        Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.
+        Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus,
+        iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum.
+        Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class
+        aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed
+        lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla
+        facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien.
+      </p>
+
+      <Datatable on:sorted={args.onSorted}>
+        <Datatable.Header>
+          <Datatable.Header.Item class={'w-50'}>Name</Datatable.Header.Item>
+          <Datatable.Header.Item>Date</Datatable.Header.Item>
+        </Datatable.Header>
+
+        <Datatable.Data>
+          <Datatable.Data.Row>
+            <Datatable.Data.Row.Item>item</Datatable.Data.Row.Item>
+            <Datatable.Data.Row.Item>today</Datatable.Data.Row.Item>
+          </Datatable.Data.Row>
+
+          <Datatable.Data.Row>
+            <Datatable.Data.Row.Item>item2</Datatable.Data.Row.Item>
+            <Datatable.Data.Row.Item>tomorrow</Datatable.Data.Row.Item>
+          </Datatable.Data.Row>
+
+          <Datatable.Data.Row>
+            <Datatable.Data.Row.Item colspan={6}>Done loading</Datatable.Data.Row.Item>
+          </Datatable.Data.Row>
+        </Datatable.Data>
+      </Datatable>
+    </Page>
+
+    <footer>
+      <a class="pr-1" href="">Terms of Service</a>
+      <a href="">Privacy Policy</a>
+    </footer>
+  </Drawer>
+</body>`,hasArgs:!0},Default:{name:"Default",template:!1,source:"",hasArgs:!1},Subtitle:{name:"Subtitle",template:!1,source:"",hasArgs:!1},Dismissible:{name:"Dismissible",template:!1,source:"",hasArgs:!1},HideForTablets:{name:"HideForTablets",template:!1,source:"",hasArgs:!1},hideForPhonesOnly:{name:"hideForPhonesOnly",template:!1,source:"",hasArgs:!1},miniMenu:{name:"miniMenu",template:!1,source:"",hasArgs:!1}},allocatedIds:["default","Button","Datatable","Drawer","Page","copyAndModifyArgs","Meta","Template","Story"]}),Ce=C.meta,Ne=["Default","Subtitle","Dismissible","HideForTablets","hideForPhonesOnly","miniMenu"],Fe=C.stories.Default,Re=C.stories.Subtitle,Te=C.stories.Dismissible,Ae=C.stories.HideForTablets,He=C.stories.hideForPhonesOnly,ke=C.stories.miniMenu;export{Fe as Default,Te as Dismissible,Ae as HideForTablets,Re as Subtitle,Ne as __namedExportsOrder,Ce as default,He as hideForPhonesOnly,ke as miniMenu};
