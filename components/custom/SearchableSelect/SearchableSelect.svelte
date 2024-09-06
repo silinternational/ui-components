a81ba38caa9ec37df00659fbd7daf6844bc7b36f
briskt <3172830+briskt@users.noreply.github.com>
2024-09-06 10:52:08 +0800

    don't make data-1p-ignore optional

--- components/custom/SearchableSelect/SearchableSelect.svelte
+++ components/custom/SearchableSelect/SearchableSelect.svelte
@@ -2,8 +2,6 @@
 import { generateRandomID } from '../../../random'
 import { createEventDispatcher } from 'svelte'
 
-/** @type {boolean} if the input should be ignored by 1Password */
-export let data_1p_ignore = false
 /** @type {object} the options to choose from. The keys are displayed in the dropdown and the value is dispatched with the change event */
 export let options = {}
 /** @type {string} the currently selected option value */
@@ -86,8 +84,8 @@ const onChange = (e) => {
 
 <label class="custom-field" style="--field-padding: {padding}; {$$props.class || ''}">
   <input
-    data-1p-ignore={data_1p_ignore}
     autocomplete="off"
+    data-1p-ignore
     {disabled}
     {maxlength}
     class="fs-14 {$$props.class || ''}"
