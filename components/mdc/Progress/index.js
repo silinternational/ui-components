0ff331e4ad7853224907bfb3dc7724cb2a371ceb
Michael Wilson <michael_wilson@sil.org>
2025-04-02 10:43:37 +0930

    use const

--- components/mdc/Progress/index.js
+++ components/mdc/Progress/index.js
@@ -2,7 +2,7 @@ import './_index.scss'
 import Circular from './Circular.svelte'
 import Linear from './Linear.svelte'
 
-let Progress = {}
+const Progress = {}
 
 Progress.Circular = Circular
 Progress.Linear = Linear
